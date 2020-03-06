9aff1233b07cec1da0511453905bb970ae0c5e05
J0S3PH-T <61877543+J0S3PH-T@users.noreply.github.com>
2020-03-06 09:39:12 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".submit").click(function(){
+    let death = $(".yourName").val();
+        let death2 = $(".foodName").val();
+        let death3 = $(".foodName2").val();
+    $(".results").append(`
+         ${death} will die in ${Number(death2) *.2} years because of ${death3}.`);
+    $(".results").fadeIn();
+});
+
+
